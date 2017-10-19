61f39a86ed60597aa9ac6f4f6aa6a2d27e2a2b64
djblake <djblake@gmail.com>
2017-10-19 02:33:21 +0100

    Add new IGListDiffOption
    IGListDiffEqualityOnly doesn't check pointer address... so you can have the same object pointer but still be marked as updated

--- Source/Common/IGListDiff.h
+++ Source/Common/IGListDiff.h
@@ -27,7 +27,8 @@ typedef NS_ENUM(NSInteger, IGListDiffOption) {
     /**
      Compare objects using `-[IGListDiffable isEqualToDiffableObject:]`.
      */
-    IGListDiffEquality
+    IGListDiffEquality,
+    IGListDiffEqualityOnly
 };
 
 /**
